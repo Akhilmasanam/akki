0502873d1dd3eef4d1cd7c6132fa41f24e0afafd
Akhil Masanam <akhil.masanam@serole.com>
2026-02-14 13:18:48 +0530

    updates

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component,signal } from '@angular/core';
+import { Component,inject,signal } from '@angular/core';
 import { RouterOutlet,RouterLink } from '@angular/router';
 import { Intro } from "./pages/intro/intro";
 
@@ -11,8 +11,29 @@ import { Intro } from "./pages/intro/intro";
 })
 export class AppComponent {
 
+  play: boolean = true;
+  isvisble: boolean = true;
+audio: HTMLAudioElement = new Audio('../assets/3-moonu-bgm-hq-background-music-appus-1_Wvl3DWx9.mp3');
 
-  isvisble:boolean=true
+
+
+  ngOnInit(): void {
+    // this.audio = new Audio('assets/3-moonu-bgm-hq-background-music-appus-1_Wvl3DWx9.mp3');
+    this.audio.loop = true; // optional if you want background music to loop
+    if(this.play){
+      this.audio.play().catch((err: any) => console.log("Autoplay blocked:", err));
+    }
+  }
+
+togglePlay() {
+    this.play = !this.play;
+    console.log(this.play)
+    if (this.play) {
+      this.audio.play().catch(err => console.log("Autoplay blocked:", err));
+    } else {
+      this.audio.pause();
+    }
+  }
 
 
   hidden(){
